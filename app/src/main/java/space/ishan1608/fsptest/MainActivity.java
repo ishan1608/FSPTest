a27aa1cebcb601749a3d84b9f42a6edc6c456ebe
Ishan <ishanatmuzaffarpur@gmail.com>
2017-10-24 23:19:02 +0530

    Sending information in a stupid manner

--- app/src/main/java/space/ishan1608/fsptest/MainActivity.java
+++ app/src/main/java/space/ishan1608/fsptest/MainActivity.java
@@ -97,26 +97,14 @@ public class MainActivity extends AppCompatActivity {
      */
     public static class PlaceholderFragment extends Fragment {
         private static final String TAG = PlaceholderFragment.class.getSimpleName();
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        int position = -1;
 
         public PlaceholderFragment() {
         }
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            Log.e(TAG, "newInstance: " + sectionNumber);
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
+        public void setPositionInfo(int positionInfo) {
+            this.position = positionInfo;
         }
 
         @Override
@@ -125,11 +113,9 @@ public class MainActivity extends AppCompatActivity {
             Log.e(TAG, "onCreateView");
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             TextView textView = rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            textView.setText(getString(R.string.section_format, position));
             return rootView;
         }
-
-
     }
 
     /**
@@ -148,7 +134,9 @@ public class MainActivity extends AppCompatActivity {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
             Log.e(TAG, "getItem: " + position);
-            return PlaceholderFragment.newInstance(position + 1);
+            PlaceholderFragment placeholderFragment = new PlaceholderFragment();
+            placeholderFragment.setPositionInfo(position + 1);
+            return placeholderFragment;
         }
 
         @Override
